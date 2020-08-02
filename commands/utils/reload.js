f878fe31ac973873ecc8429dc92637073a12015f
Alexandra <pinkasan43110@gmail.com>
2020-08-01 20:34:05 -0400

    send backup data on reload

--- commands/utils/reload.js
+++ commands/utils/reload.js
@@ -1,10 +1,13 @@
 var {save_all, buffs, members, skills, items, sent_emoji,
   save, load, alias, get_alias,prefix, token, admin_roles} = require('../../globals.js')
 const confirm_message = require('../../components/confirm_message.js')
+const fs = require('fs')
+const Discord = require('discord.js')
 
 module.exports = {
   name: "reload",
   desc: "Reload the bot's save data.",
+  usage: "!reload (optional:) <save>, <nosave>, <nobackup>",
   args: false,
   admin: true,
   async execute(message, args) {
@@ -18,12 +21,19 @@ module.exports = {
       }
     }
     try {
+      if (!args.includes("nobackup")) {
+        save_all(true)
+        let attachment = new Discord.MessageAttachment('./globals_temp.json')
+        await message.author.send(`${message.client.user.tag} data backup!`, attachment)
+        fs.unlinkSync("./globals_temp.json")
+      }
       load(buffs, "buffs")
       load(alias, "alias")
       load(skills, "skills")
       load(items, "items")
       load(sent_emoji, "sent_emoji")
       load(members, "members")
+      await message.channel.send("```Data Reloaded.```")
     } catch (e) {
       await message.channel.send("```Error: data is invalid.```")
       return console.log(e)
